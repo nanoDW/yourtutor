3c62ae5b808342a50cc578415889745940ec4830
Sylwia <sylwia.fluder@applover.pl>
2020-04-25 21:08:12 +0200

    Create start learning button

---
+++ client/src/components/StartButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="button button-active">Start learning</button>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "StartButton",
+  methods: {
+    ...mapActions({}),
+  },
+};
+</script>
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -4,11 +4,15 @@
       <v-row>
         <Logo @click.native="goToWelcomeScreen" />
       </v-row>
-      <v-row class="px-6">
-        <User :user="user" />
+      <v-row class="px-6 mt-12">
+        <v-card v-if="user.name" class="pt-8" style="border-radius: 1.75rem !important;">
+          <User :user="user" />
+        </v-card>
       </v-row>
     </v-col>
-    <v-col cols="6">Center</v-col>
+    <v-col cols="6" class="my-12">
+      <AppContainer />
+    </v-col>
     <v-col cols="3">Right side</v-col>
   </v-row>
 </template>
@@ -17,9 +21,10 @@
 import { mapActions, mapState } from "vuex";
 import Logo from "../shared/Logo";
 import User from "../components/User";
+import AppContainer from "../components/Container";
 
 export default {
-  components: { Logo, User },
+  components: { Logo, User, AppContainer },
   methods: {
     ...mapActions(["changeStep"]),
     goToWelcomeScreen() {
@@ -31,6 +36,11 @@ export default {
     ...mapState({
       user: (state) => state.user,
     }),
+  },
+  created() {
+    if (!this.user.name) {
+      this.$router.push("/");
+    }
   },
 };
 </script>
